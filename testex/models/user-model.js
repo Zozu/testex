05783b7fc5584d0f9bf2857c5c0920d5907039f5
Max Selekh <max.selekh@gmail.com>
2016-05-29 07:13:09 +0300

    user model and CRUD routes

---
+++ testex/models/user-model.js
@@ -0,0 +1,108 @@
+var mongoose = require('mongoose'),
+	bcrypt = require('bcrypt-nodejs');
+
+var toHash = pass => bcrypt.hashSync(pass, bCrypt.genSaltSync(10), null);
+
+var userSchema = mongoose.Schema({
+    username: { 
+    	type: String, 
+    	required: true,
+    	minlength: 3,
+    	maxlength: 20
+    },
+    email: { 
+    	type: String, 
+    	required: true,
+    	index: { unique: true },
+    	match: /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i 
+    },
+    password: { 
+    	type: String, 
+    	required: true,
+    	minlength: 6,
+    	maxlength: 20, 
+    	set: toHash
+    },
+    admin: { 
+    	type: Boolean,
+    	default: false
+    }
+});
+
+var User = mongoose.model('User', userSchema);
+
+User.addUser = userFromClient => {
+	var addUserPromise = User.findOne({'email': user.email}).exec();
+
+	return addUserPromise
+		.then((user) => {
+			if(user) throw new Error(false);
+			else {
+				var newUser = new User(userFromClient);
+				return newUser.save();
+			}
+		})
+		.then(savedUser => savedUser)
+		.catch(err => err);
+};
+
+User.getAll = () => {
+	var getAllPromise = User.find({}).exec();
+	return getAllPromise
+		.then(users => users)
+		.catch(err => err);
+    /*var userMap = {};*/
+
+    /*users.forEach(function(user) {
+      userMap[user._id] = user;
+    });*/ 
+};
+
+User.findUser = email => {
+	var findUserPromise = User.findOne({'email': email}).exec();
+
+	return findUserPromise
+		.then(user => {
+	        if (!user){
+	            throw new Error(false);                 
+	        }
+	        return user;
+		})
+		.catch(err => err);
+};
+
+User.updateUser = (email, userFromClient) => {
+	return this.findUser(email)
+		.then(user => {
+			user.username = userFromClient.username;
+			user.password = userFromClient.password;
+			user.admin = userFromClient.admin;
+			return user.save();
+		})
+		.then(savedUser => savedUser)
+		.catch(err => err);
+};
+
+User.deleteUser = email => {
+	return User.findOneAndRemove({'email': email}).exec();
+};
+
+User.tryLogin = (email, password) => {
+	var loginPromise = User.findOne({'email': email}).exec();
+
+	return	loginPromise
+		.then(user => {
+	        if (!user){
+	            console.log('User Not Found with email ' + email);
+	            throw new Error(false);             
+	        }
+	        if (bcrypt.compareSync(password, user.password)){
+	            console.log('Invalid Password');
+	            throw new Error(false);
+	        }
+	        return user;
+	    })
+	    .catch(err => err);
+};
+
+module.exports = User;
--- testex/routes/api.js
+++ testex/routes/api.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	router = express.Router(),
+	User = require('../models/user-model');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-	res.send('respond with a resource');
+router.get('/user/all', (req, res, next) => {
+	User.getAll()
+		.then(users => {res.status(200).end(users);})
+		.catch(err => {res.status(500).end(err);});
 });
+
+router.get('/user/:email', (req, res, next) => {
+	User.findUser(req.params.email)
+		.then(user => res.status(200).end(user);)
+		.catch(err => {
+			if (err === false) res.status(404).end('Not Found');
+			else res.status(500).end(err);
+		});
+});
+
+router.put('/user', (req, res, next) => {
+	console.log("AAAAAAAAAAAAAAAAAAAAAAAAA");
+	console.log(req.body.user);
+	User.addUser(req.body.user)
+		.then(savedUser => {res.status(200).end(savedUser);})
+		.catch(err => {
+			if (err === false) res.status(403).end('Already exist');
+			else res.status(500).end(err);
+		});
+});
+
+router.update('/user/:email', (req, res, next) => {
+	User.updateUser(req.params.email, req.body.user)
+		.then(updatedUser => {res.status(200).end(updatedUser);})
+		.catch(err => {
+			if (err === false) res.status(404).end('Not Found');
+			else res.status(500).end(err);
+		});
+});
+
+router.delete('/user/:email', (req, res, next) => {
+	User.deleteUser(req.params.email)
+		.then(deletedUser => {res.status(200).end(deletedUser);})
+		.catch(err => {res.status(500).end(err);});
+})
 
 module.exports = router;
